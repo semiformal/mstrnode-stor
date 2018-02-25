1e5a7dac50fe89e5ae45c1af53a7d0883fdbe770
semiformal <MrKaishi@hotmail.com>
2018-02-25 00:50:31 -0800

    First rev of endpoint

--- graphapi.ts
+++ graphapi.ts
@@ -4,13 +4,55 @@ import {
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLString,
-    GraphQLNonNull
+    GraphQLNonNull,
+    GraphQLFloat,
+    GraphQLInt,
+    GraphQLInputObjectType,
 }
     from 'graphql';
 import { Handler, Context, Callback } from 'aws-lambda';
+import * as _ from 'lodash';
 import * as AWS from 'aws-sdk';
+import { read } from 'fs';
+import { callbackify } from 'util';
+
+
+// TODO: don't hard code this
+AWS.config.update({ region: 'us-east-1' });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const PriceHistoryType = new GraphQLObjectType({
+    name: 'PriceHistory',
+    description: 'The history of coin price compared across multiple trades.',
+    fields: () => ({
+        coinPairKey: { type: new GraphQLNonNull(GraphQLString) },
+        timeKey: { type: new GraphQLNonNull(GraphQLString) },
+        exchange: { type: GraphQLString },
+        finalPrice: { type: GraphQLFloat },
+        volume24hr: { type: GraphQLFloat },
+        marketSize: { type: GraphQLFloat },
+        orderType: { type: GraphQLString },
+        startCoin: { type: GraphQLString },
+        endCoin: { type: GraphQLString }
+    })
+});
+
+const PriceHistoryInputType = new GraphQLInputObjectType({
+    name: 'PriceHistoryInput',
+    description: 'Used for creating new PriceHistories',
+    fields: () => ({
+        coinPairKey: { type: new GraphQLNonNull(GraphQLString) },
+        timeKey: { type: new GraphQLNonNull(GraphQLString) },
+        exchange: { type: GraphQLString },
+        finalPrice: { type: GraphQLFloat },
+        volume24hr: { type: GraphQLFloat },
+        marketSize: { type: GraphQLFloat },
+        orderType: { type: GraphQLString },
+        startCoin: { type: GraphQLString },
+        endCoin: { type: GraphQLString }
+    })
+});
 
 
 // Here we declare the schema and resolvers for the query
@@ -19,57 +61,96 @@ const schema = new GraphQLSchema({
         name: 'RootQueryType', // an arbitrary name
         fields: {
             // the query has a field called 'greeting'
-            greeting: {
-                // we need to know the user's name to greet them
-                args: { firstName: { name: 'firstName', type: new GraphQLNonNull(GraphQLString) } },
-                // the greeting message is a string
-                type: GraphQLString,
-                // resolve to a greeting message
-                resolve: (parent, args) => getGreeting(args.firstName)
+            getPriceHistory: {
+                args: {
+                    coinPairKey: { type: new GraphQLNonNull(GraphQLString) },
+                    timeKey: { type: new GraphQLNonNull(GraphQLString) }
+                },
+                type: PriceHistoryType,
+                resolve: (parent, args) => getPriceHistory(args.coinPairKey, args.timeKey)
+            },
+            getLatestPriceHistory: {
+                args: {
+                    coinPairKey: { type: new GraphQLNonNull(GraphQLString) }
+                },
+                type: PriceHistoryType,
+                resolve: (parent, args) => getLatestPriceHistory(args.coinPairKey)
             }
         }
     }),
     mutation: new GraphQLObjectType({
         name: 'RootMutationType', // an arbitrary name
         fields: {
-            changeNickname: {
+            upsertPriceHistory: {
                 args: {
-                    // we need the user's first name as well as a preferred nickname
-                    firstName: { name: 'firstName', type: new GraphQLNonNull(GraphQLString) },
-                    nickname: { name: 'nickname', type: new GraphQLNonNull(GraphQLString) }
+                    priceHistory: { type: PriceHistoryInputType }
                 },
-                type: GraphQLString,
-                // update the nickname
-                resolve: (parent, args) => changeNickname(args.firstName, args.nickname)
+                type: PriceHistoryType,
+                resolve: (parent, args) => upsertPriceHistory(args)
             }
         }
     })
 });
 
-// This method just inserts the user's first name into the greeting message.
-const getGreeting = firstName => promisify(callback =>
+const getPriceHistory = (coinPairKey, timeKey) => promisify(callback =>
     dynamoDb.get({
         TableName: process.env.DYNAMODB_TABLE,
-        Key: { firstName },
+        Key: { 'coinPairKey': coinPairKey, 'timeKey': timeKey },
+    }, callback))
+    .then((result: any) => result.Item);
+
+const getLatestPriceHistory = (coinPairKey) => promisify(callback =>
+    dynamoDb.query({
+        TableName: process.env.DYNAMODB_TABLE,
+        KeyConditionExpression: '#cpk = :cpk',
+        ExpressionAttributeNames: {
+            '#cpk': 'coinPairKey'
+        },
+        ExpressionAttributeValues: {
+            ':cpk': coinPairKey
+        },
+        Limit: 1,
+        ScanIndexForward: false
     }, callback))
     .then((result: any) => {
-        if (!result.Item) {
-            return firstName
-        }
-        return result.Item.nickname
-    })
-    .then(name => `Hello, ${name}.`);
+        return result.Items[0];
+    });
 
-const changeNickname = (firstName, nickname) => promisify(callback =>
-    dynamoDb.update({
+const upsertPriceHistory = (args) => promisify(callback => {
+    let priceHistory = args.priceHistory;
+    let priceHistoryNoKeys = _.omit(priceHistory, ['coinPairKey', 'timeKey']);
+
+    let update = _.reduce(priceHistoryNoKeys, (result: any, value, key) => {
+        result += ' ' + key + ' = :' + key + ','
+        return result;
+    }, 'SET');
+
+    let values = _.reduce(priceHistoryNoKeys, (result: any, value, key) => {
+        result[':' + key] = value;
+        return result;
+    }, {});
+
+    update = update.slice(0, -1);
+    let params = {
         TableName: process.env.DYNAMODB_TABLE,
-        Key: { firstName },
-        UpdateExpression: 'SET nickname = :nickname',
-        ExpressionAttributeValues: {
-            ':nickname': nickname
-        }
-    }, callback))
-    .then(() => nickname);
+        Key: { 'coinPairKey': priceHistory.coinPairKey, 'timeKey': priceHistory.timeKey },
+        UpdateExpression: update,
+        ExpressionAttributeValues: values
+    };
+    return dynamoDb.update(params, callback);
+}).then((priceHistory) => {
+    return priceHistory;
+});
+// const changeNickname = (firstName, nickname) => promisify(callback =>
+//     dynamoDb.update({
+//         TableName: process.env.DYNAMODB_TABLE,
+//         Key: { firstName },
+//         UpdateExpression: 'SET nickname = :nickname',
+//         ExpressionAttributeValues: {
+//             ':nickname': nickname
+//         }
+//     }, callback))
+//     .then(() => nickname);
 
 
 const promisify = foo => new Promise((resolve, reject) => {
@@ -83,18 +164,36 @@ const promisify = foo => new Promise((resolve, reject) => {
 })
 
 // We want to make a GET request with ?query=<graphql query>
+// Or a POST request with body = <graphql query>
 // The event properties are specific to AWS. Other providers will differ.
 const graphapi: Handler = (event, context, callback) => {
-    console.log('it worked');
-    return graphql(schema, event.queryStringParameters.query)
+
+    let graphqlQuery: string;
+    if (event.requestContext.httpMethod === "POST") {
+        let body = JSON.parse(event.body);
+        if (!_.isNil(body.query)) {
+            graphqlQuery = body.query;
+        } else {
+            callback(null, {statusCode: 500, body: `ERROR: no query element on request ${event.body}` });  
+        }        
+    } else if (event.requestContext.httpMethod === "GET") {
+        graphqlQuery = event.queryStringParameters.query;
+    } else {
+        let errorString = `ERROR: wrong httpMethod: ${event.requestContext.httpMethod} `
+        console.error(errorString);
+        callback(errorString);
+    }
+    console.log('Calling graphql with request: ', graphqlQuery);
+    return graphql(schema, graphqlQuery)
         .then(
-        result => callback(null, { statusCode: 200, body: JSON.stringify(result) }),
+        result => {
+            console.log(`returning result:`);
+            console.log(result);
+            return callback(null, { statusCode: 200, body: JSON.stringify(result) })
+        },
         err => callback(err)
         );
 }
-
-
-
 
 
 export { graphapi };
--- index.ts
+++ index.ts
@@ -1,4 +1,97 @@
 import { graphapi } from './graphapi';
+import * as moment from 'moment';
+
+const cb = (err, result) => {
+    console.log(err);
+    console.log(result);
+    let body;
+    let errors;
+    if (Boolean(result)) {
+        body = JSON.parse(result.body);
+        errors = body.errors;
+        if (!!errors) {
+            console.log(errors);
+        }
+    }
+}
+
+let now = moment().utc().format();
+
+//let now = "2018-02-25T06:00:00Z";
+
+let createQuery = `
+        mutation 
+        {
+            a1: upsertPriceHistory(
+                priceHistory: {
+                    coinPairKey: "BTC"
+                    timeKey: "2017-02-22T02:00:00Z"
+                    exchange: "GDAX"
+                    finalPrice: 29
+                }
+            ) { coinPairKey }
+            a2: upsertPriceHistory(
+                priceHistory: {
+                    coinPairKey: "BTC"
+                    timeKey: "${now}"
+                    exchange: "GDAX"
+                    finalPrice: 55
+                }
+            ) { coinPairKey }
+        }
+        `;
+let simpleCreateQuery = `
+        mutation 
+        {
+            upsertPriceHistory(
+                priceHistory: {
+                    coinPairKey: "BTC"
+                    timeKey: "2018-02-22T02:00:00Z"
+                    exchange: "GDAX"
+                    finalPrice: 29
+                }
+            ) { coinPairKey }
+        }
+        `;
+
+let get = {
+    'queryStringParameters': {
+        'query': `
+        query 
+        {
+            getPriceHistory(
+                coinPairKey: \"BTC\"
+                timeKey: \"${now}\"
+            ) { 
+                coinPairKey
+                finalPrice
+             }
+        }
+        `
+    }
+};
+
+let getLatestQuery = `
+        query 
+        {
+            getLatestPriceHistory(coinPairKey: \"BTC\") {coinPairKey exchange finalPrice}
+        }
+        `;
+
+let typeName = `{ __typename }`;
+
+let event = {
+    'requestContext': {
+        'httpMethod': 'POST'
+    },
+    'body': {
+        'query': typeName
+    }
+};
+
+let context = null;
+process.env.DYNAMODB_TABLE = 'pricehistory-dev';
+process.env.REGION = 'us-east-1';
 
 //TODO: fake out parameters so can actually test using debugger
-graphapi();
+graphapi(event, context, cb);
